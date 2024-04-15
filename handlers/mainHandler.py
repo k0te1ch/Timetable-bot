a04f51f969995c94f9b7d14b5b9c91b1c8881af3
ShyDamn <den03062003@gmail.com>
2024-04-15 11:32:51 +0300

    "Revert effects of previous git merge and apply minor fix" Add feedback

--- handlers/mainHandler.py
+++ handlers/mainHandler.py
@@ -168,7 +168,6 @@ async def timetableForDay(callback: CallbackQuery, username: str) -> None:
     await callback.message.answer(text=schedule_for_week_str.strip())
     await callback.answer("Ваше расписание")
 
-
 @router.callback_query(F.data == "settings")
 async def settings(callback: CallbackQuery, username: str) -> None:
     logger.opt(colors=True).debug(f"[<y>{username}</y>]: Called <b>settings</b> callback")
@@ -182,11 +181,11 @@ async def settings(callback: CallbackQuery, username: str) -> None:
     if existingUser is None:
         return await callback.answer("Вы не зарегистрированы!")
     keyboard = InlineKeyboardBuilder()
+    keyboard.row(InlineKeyboardButton(text="Введите сообщение для обратной связи", callback_data="feedback"))
     keyboard.row(InlineKeyboardButton(text="Удалить аккаунт", callback_data="delete_user"))
     keyboard.row(InlineKeyboardButton(text="Назад", callback_data="menu"))
     await callback.message.edit_text("Настройки", reply_markup=keyboard.as_markup())
     return await callback.answer()
-
 
 @router.callback_query(F.data == "delete_user")
 async def deleteUser(callback: CallbackQuery, username: str, state: FSMContext) -> None:

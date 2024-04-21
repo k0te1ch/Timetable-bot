3b759da6800651304784d9ad2b6ab78152cdc453
ShyDamn <den03062003@gmail.com>
2024-04-21 18:54:52 +0300

    Fixed Feedback

--- handlers/feedbackHandler.py
+++ handlers/feedbackHandler.py
@@ -13,17 +13,18 @@ router = Router(name="feedbackHandler")
 async def feedback(callback: CallbackQuery, state: FSMContext):
     await callback.answer("Введите сообщение для обратной связи")
     await callback.message.answer("Введите сообщение для обратной связи")
-    
+    await state.update_data(user_chat_id=callback.message.chat.id)
+
     await state.set_state(Feedback.message)
 
 
 @router.message(StateFilter(Feedback.message))
-async def feedback_message(message: Message, state: FSMContext, username, bot):
-    logger.opt(colors=True).debug(f"[<y>{username}</y>]: Send Feedback")
-    await bot.send_message(ADMIN_CHAT_ID, message.text)
+async def feedback_message(message: Message, state: FSMContext, bot):
+    user_chat_id = (await state.get_data()).get("user_chat_id")
+    forwarded_message = await message.forward(ADMIN_CHAT_ID)
+    logger.debug(f"Переслано сообщение от пользователя {user_chat_id} в чат {ADMIN_CHAT_ID}")
     await message.answer("Спасибо за обратную связь!")
     await state.clear()
-
 
 @router.callback_query(F.data == "cancel", StateFilter(Feedback))
 async def cancel(callback: CallbackQuery, state: FSMContext, language: str, username: str) -> None:
--- handlers/mainHandler.py
+++ handlers/mainHandler.py
@@ -166,6 +166,7 @@ async def settings(callback: CallbackQuery, username: str, db) -> None:
     if existingUser is None:
         return await callback.answer("Вы не зарегистрированы!")
     keyboard = InlineKeyboardBuilder()
+    keyboard.row(InlineKeyboardButton(text="Обратная связь", callback_data="feedback"))
     keyboard.row(InlineKeyboardButton(text="Удалить аккаунт", callback_data="delete_user"))
     keyboard.row(InlineKeyboardButton(text="Назад", callback_data="menu"))
     await callback.message.edit_text("Настройки", reply_markup=keyboard.as_markup())
